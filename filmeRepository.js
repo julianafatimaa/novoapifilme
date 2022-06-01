7e82cea749045b5a33d933c9a3d39b94914ff1d6
Esterqc <105655984+Esterqc@users.noreply.github.com>
2022-06-01 01:54:18 -0300

    Add files via upload

---
+++ filmeRepository.js
@@ -0,0 +1,71 @@
+import { con } from "./connection.js";
+
+export async function inserirFilme(filme) {
+
+const comando =
+
+`
+
+INSERT INTO tb_filme (id_usuario, nm_filme, ds_sinopse, vl_avaliacao, dt_lancamento, bt_disponivel)
+
+VALUES (?, ?, ?, ?, ?, ? )
+
+`
+
+const [resposta] = await con.query(comando, [filme.usuario, filme.nome, filme.sinopse, filme.avaliacao, filme.lancamento, filme.disponivel]);
+
+filme.id = resposta.insertId;
+
+return filme;
+
+}
+
+export  async function alteraimagem(imagem, id){
+    const comando =
+    `UPDATE tb_filme 
+    SET img_filme     = ?
+    WHERE id_filme    = ?`;
+
+    const [resposta] =  await con.query(comando, [imagem,id]);
+      return resposta.affectedRows;
+}
+
+export async function removerFilme(id) {
+    const comando = `
+    DELETE FROM   tb_filme 
+          WHERE   id_filme = ?`;
+
+  const [resposta] = await con.query(comando, [id]);
+  return resposta.affectedRows;
+
+}
+
+export async function buscarPorid(id){
+  const comando =
+ ` SELECT id_filme		id,
+	  nm_filme	     nome,
+       vl_avaliacao		avaliacao,
+	  ds_sinopse		sinopse,
+       dt_lancamento	lancamento,
+       bt_disponivel	disponivel,
+       img_filme         capa
+    FROM tb_filme
+  WHERE id_filme			= 1`;
+    
+     const[linhas] = await con.query(comando,[id]);
+        return linhas[0];
+   }
+
+   export async function buscarPorNome(nome){
+     const comando =
+     `SELECT id_filme			id,
+      nm_filme			nome,
+        vl_avaliacao		avaliacao,
+        dt_lancamento	lancamento,
+        bt_disponivel	disponivel
+   FROM tb_filme `
+ 
+
+        const[linhas] = await con.query(comando,[`%${nome}%`]);
+        return linhas;      
+      }
